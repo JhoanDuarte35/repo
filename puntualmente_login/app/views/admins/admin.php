a42926891c86378aa585ff85f5135408e62b16eb
JhoanDuarte35 <81330498+JhoanDuarte35@users.noreply.github.com>
2023-02-10 16:51:05 -0500

    todo

---
+++ puntualmente_login/app/views/admins/admin.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo controlador::$rutaAPP?>app/views/admins/css/menu.css">
+    <title>Admin</title>
+</head>
+<body>
+    <div>
+    <h1><?php echo $_SESSION["username"]?></h1>
+    <a href="<?php echo controlador::$rutaAPP?>index.php?action=cerrar">cerrar sesión</a>
+    </div>
+
+    <div id="accordian">
+	<ul>
+		<!-- we will keep this LI open by default -->
+		<li class="active">
+			<h3><span class="icon-tasks"></span>Evaluaciones</h3>
+			<ul>
+				<li><a href="<?php echo controlador::$rutaAPP?>index.php?action=admev">Admin Evaluacion</a></li>
+				<li><a href="#">Crear Evaluacion</a></li>
+				<li><a href="#">Listar Reportes</a></li>
+			</ul>
+		</li>
+	</ul>
+</div>
+
+<!-- prefix free to deal with vendor prefixes -->
+<script src="http://thecodeplayer.com/uploads/js/prefixfree-1.0.7.js" type="text/javascript" type="text/javascript"></script>
+
+<!-- jQuery -->
+<script src="http://thecodeplayer.com/uploads/js/jquery-1.7.1.min.js" type="text/javascript"></script>
+    <!-- <a href="http://localhost/puntualmente_login/index.php?action=operarios">Intefaz operarios</a> -->
+    
+    
+</body>
+</html>
---
+++ puntualmente_login/app/views/test/admin_eva/admin_eva.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Gestion de preguntas</h1>
+    <div>
+        <form action="">
+        <label for="preguntas">Pregunta</label>
+        <input type="text" placeholder="ingresa la pregunta">
+        <label for="puntuacion">Puntuacion</label>
+        <input type="text" placeholder="Ingresa la puntuacion de la pregunta">
+        <input type="submit" value="Agregar pregunta">
+        </form>
+    </div>
+</body>
+</html>
